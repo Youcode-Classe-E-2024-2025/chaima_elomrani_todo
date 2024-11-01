9df60002ae8bb3ab598cb2c3673b113e189a87a4
chaima-syc <chaimaelomrani6@gmail.com>
2024-11-01 21:30:41 +0100

    realisation de l'affichage des taches ajoutée par l'utilisateur dans la zone de todo

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
 // ********************** form *****************************
 const addbtn = document.getElementById("addbtn");
+const formContainer=document.getElementById("form-container");
 const form=document.getElementById("form");
 const exit=document.getElementById("exit");
+const todoAdd=document.querySelector('.todoAdd');
+const doingAdd=document.querySelector('.doingAdd')
+const line = document.querySelector('.line');
 
 addbtn.addEventListener('click',function(){
-    form.style.display="block";
+    formContainer.style.display="block";
 });
 
 exit.addEventListener('click',function(){
-    form.style.display="none";
+    formContainer.style.display="none";
 });
  
 // ********************** array *****************************
 const tacheArray = [] ;
 const submitBtn = document.getElementById("submit");
-submitBtn.addEventListener('click',function(){
+submitBtn.addEventListener('click',function(e){
+    e.preventDefault()
     const title = document.getElementById("title").value;
     const description =document.getElementById("description").value;
     const date =document.getElementById("date").value;
@@ -29,16 +34,63 @@ submitBtn.addEventListener('click',function(){
         statut : statut,
     };
 
+    console.log(newtask);
+
     tacheArray.push(newtask);
-    console.log(-8)
-    
+  
+    console.log(tacheArray);
 
+    form.reset();
+    formContainer.style.display="none";
+
+   
+    // const card = document.createElement('div');
+    // card.classList.add('doing-card');
+    // card.appendChild(priority);
+    affichage(tacheArray);
+    lineColor(tacheArray);
+    affichageDoing(tacheArray);
 })
+function lineColor(tacheArray){
+    for(let i= 0 ; i<tacheArray.length; i++){
+        if(tacheArray[i].priority === 'P1'){
+            line.style.backgroundColor = 'red';
+        }else if(tacheArray[i].priority === 'P2'){
+            line.style.backgroundColor = 'orange';
+        }else if(tacheArray[i].priority === 'P3'){
+            line.style.backgroundColor = 'green';
+        }
+    }
+}
 
+function affichage(tacheArray){
+     todoAdd.innerHTML='';
+    for(let i= 0 ; i<tacheArray.length; i++){
+        if(tacheArray[i].statut === 'todo'){
 
+            let lineColor;
+            if (tacheArray[i].priority === 'P1') {
+                lineColor = 'red';
+            } else if (tacheArray[i].priority === 'P2') {
+                lineColor = 'orange';
+            } else if (tacheArray[i].priority === 'P3') {
+                lineColor = 'green';
+            }
 
+            todoAdd.innerHTML +=`
+            <div class="tache">
+                        <div class="tache-icon">
+                            <div class="line" style="background-color: ${lineColor};"></div>
+                            <button id="btn" style="background-color:white; border: none; "><i class='bx bxs-trash'
+                                    style="padding-right: 20px;"></i></button>
+                        </div>
+                        <h4 class="title">${tacheArray[i].title}</h4>
+                    </div>
+          `
+    }
+        }
+    }
 
-// **********************************************************
-
-
+         
+    
 
