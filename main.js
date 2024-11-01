d4459882e4ccb799589ae32c484c3876c46820e7
chaima-syc <chaimaelomrani6@gmail.com>
2024-11-01 12:09:23 +0100

    continution des cartes des taches

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+// ********************** form *****************************
 const addbtn = document.getElementById("addbtn");
 const form=document.getElementById("form");
 const exit=document.getElementById("exit");
+
 addbtn.addEventListener('click',function(){
     form.style.display="block";
 });
@@ -8,4 +10,35 @@ addbtn.addEventListener('click',function(){
 exit.addEventListener('click',function(){
     form.style.display="none";
 });
+ 
+// ********************** array *****************************
+const tacheArray = [] ;
+const submitBtn = document.getElementById("submit");
+submitBtn.addEventListener('click',function(){
+    const title = document.getElementById("title").value;
+    const description =document.getElementById("description").value;
+    const date =document.getElementById("date").value;
+    const priority =document.getElementById("priority").value;
+    const statut =document.getElementById("statut").value;
+
+    const newtask ={
+        title: title,
+        description :description,
+        date :date,
+        priority :priority,
+        statut : statut,
+    };
+
+    tacheArray.push(newtask);
+    console.log(-8)
+    
+
+})
+
+
+
+
+// **********************************************************
+
+
 
